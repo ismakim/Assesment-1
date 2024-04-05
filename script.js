1d0678055abb0e5f7e41afd5e8b4e1a236d1aac0
Ismail Abdul Hakim <ismakim22@gmail.com>
2024-04-05 14:58:24 +0700

    next 1.1

---
+++ script.js
@@ -0,0 +1,11 @@
+let buku = [
+    {"nama": "Shelock", "Jumlah":2},
+    {"nama": "Lupin", "Jumlah":2},
+    {"nama": "Aladin", "Jumlah":2}
+];
+
+function checkAvailability() {
+    let jumlahInput = document.getElementById("Jumlah").value;
+    let pilihanBuku = document.getElementById("pilihan").value;
+
+}
