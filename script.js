8f49656ba4b8d30ac4390a80e4f9b2773cfa43b0
Ismail Abdul Hakim <ismakim22@gmail.com>
2024-04-05 15:02:50 +0700

    next 1.2

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let buku = [
     {"nama": "Lupin", "Jumlah":2},
     {"nama": "Aladin", "Jumlah":2}
 ];
+
+function checkAvailability() {
+    let jumlahInput = document.getElementById("Jumlah").value;
+    let pilihanBuku = document.getElementById("pilihan").value;
+
+    if (jumlahInput === "") {
+        alert("Jumlah harus diisi");
+        return;
+    }
+
+}
