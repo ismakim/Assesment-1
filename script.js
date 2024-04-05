3da141a88f12ecbfea1a1baece7ebebcbf081eae
Ismail Abdul Hakim <ismakim22@gmail.com>
2024-04-05 15:18:22 +0700

    next 1.4

--- script.js
+++ script.js
@@ -13,4 +13,20 @@ function checkAvailability() {
         return;
     }
 
+    let bukuDitemukan = false;
+    for (let i = 0; i < buku.length; i++) {
+        if (buku[i].nama === pilihanBuku)  {
+            bukuDitemukan = true;
+            if (parseInt(jumlahInput)> buku[i].Jumlah) {
+                alert("Jumlah buku tidak tersedia");
+            } else {
+            alert("Silahkan ambil buku anda");
+        }
+            break;
+        }
+    }
+
+    if (!bukuDitemukan) {
+        alert("Buku tidak ditemukan");
+    }
 }
